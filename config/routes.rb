848369500b3df547dd1cac26d2eb714621879597
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-07-30 13:11:14 +0300

    clear sheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :users do
-    get 'omniauth_callbacks/facebook'
-  end
   devise_for :users,
              :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 

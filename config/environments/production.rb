8b04c9e6b485a2d2cc63488a380687b1a85f816d
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-09-03 16:16:21 +0300

    mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,19 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "gallery_#{Rails.env}"
 
-  config.action_mailer.default_url_options = { host: 'https://gallery-project-1st.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'gallery-project-1st.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
-      domain: 'gallery-project-1st.herokuapp.com',
+      domain: 'gmail.com',
       user_name: ENV['USERNAME'],
       password: ENV['PASSWORD'],
       authentication: :plain,
       enable_starttls_auto: true
   }
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

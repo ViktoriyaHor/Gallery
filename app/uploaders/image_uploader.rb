e101973cf43d591d5315a41584f2a8cbf71ea995
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-09-05 15:41:05 +0300

    edit image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,10 +16,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def store_dir
-    'public/my/upload/directory'
-  end
-
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:

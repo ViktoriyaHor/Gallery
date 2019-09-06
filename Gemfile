7f0cff360d5aaf9fbcbf1be350572a704674c932
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-09-06 09:55:14 +0300

    Add Amazon S3 for production

--- Gemfile
+++ Gemfile
@@ -64,9 +64,9 @@ end
 
 group :production do
   gem 'rails_12factor', '0.0.2'
-  gem "fog-aws"
-end
 
+end
+gem "fog-aws"
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,7 @@ if Rails.env.production?
         provider:              'AWS',
         aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
         aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+        region: ENV['AWS_REGION']
     }
     config.fog_directory  = ENV['AWS_BUCKET']
   end

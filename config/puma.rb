7387f4d22b86bf9063c275c05862f60182a0af77
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-09-19 16:15:06 +0300

    change puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,30 @@ port        ENV.fetch("PORT") { 3000 }
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
+# Config Parameters
+app_root = ENV['APP_ROOT'] || '.'
+rails_env = ENV['RAILS_ENV'] || 'production'
+ip_addr = ENV['IP_ADDR'] || 'localhost'
+port = ENV['PORT'] || 3000
+ssl_key = ENV['SSL_KEY']
+ssl_cert = ENV['SSL_CERT']
 
+# Set Puma parameters
+preload_app!
+rackup DefaultRackup
+environment rails_env
+pidfile "#{app_root}/tmp/pids/puma.pid"
+state_path "#{app_root}/tmp/pids/puma.state"
+threads 0, 16
+workers 2
+stdout_redirect "#{app_root}/log/puma.log", "#{app_root}/log/puma.err", true if rails_env.downcase == 'production'
+
+# HTTP or HTTPS
+if ssl_key.present? && ssl_cert.present?
+  bind "ssl://#{ip_addr}:#{port}?key=#{ssl_key}&cert=#{ssl_cert}"
+else
+  bind "tcp://#{ip_addr}:#{port}"
+end
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.

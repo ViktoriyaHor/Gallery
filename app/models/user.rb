8e5e1be5e788789db545f172781db0c6d5ffa2e8
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-09-24 15:53:53 +0300

    delete omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,15 +14,15 @@ class User < ApplicationRecord
   has_many :images, dependent: :destroy
   has_many :logging_user_actions, dependent: :destroy
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid, username: auth.info.name).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.username = auth.info.name
-      user.password = Devise.friendly_token[0, 20]
-    end
-  end
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid, username: auth.info.name).first_or_create do |user|
+  #     user.provider = auth.provider
+  #     user.uid = auth.uid
+  #     user.email = auth.info.email
+  #     user.username = auth.info.name
+  #     user.password = Devise.friendly_token[0, 20]
+  #   end
+  # end
 
   def self.logins_before_captcha
     3

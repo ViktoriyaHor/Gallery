11930b03624473ec46149e9b9a765d5b2f33410f
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-10-15 13:29:03 +0300

    logging commit

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -59,9 +59,9 @@ class Users::SessionsController < Devise::SessionsController
     params[:recaptcha_challenge_field]
   end
 
-  def action_sign_in
-    LoggingUserAction.new(:user_id=>current_user.id, :action_id=>"#{Action.find_by_action_type('user_sign_in').id}", :action_path=>request.original_url).save if user_signed_in?
-  end
+  # def action_sign_in
+  #   LoggingUserAction.new(:user_id=>current_user.id, :action_id=>"#{Action.find_by_action_type('user_sign_in').id}", :action_path=>request.original_url).save if user_signed_in?
+  # end
 
   def action_sign_out
     LoggingUserAction.new(:user_id=>current_user.id, :action_id=>"#{Action.find_by_action_type('user_sign_out').id}", :action_path=>request.original_url).save if user_signed_in?
